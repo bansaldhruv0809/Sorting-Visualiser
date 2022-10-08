e97a92d14baaf9b80e610b6dbf8ad11bbd271906
bansaldhruv0809 <84713243+bansaldhruv0809@users.noreply.github.com>
2022-10-08 18:59:45 +0530

    Update selection.js

--- js_files/selection.js
+++ js_files/selection.js
@@ -5,32 +5,32 @@ async function selection(){
         console.log('In ith loop');
         let min_index = i;
         // Change color of the position to swap with the next min
-        ele[i].style.background = 'blue';
+        ele[i].style.background = '#0d6efd';
         for(let j = i+1; j < ele.length; j++){
             console.log('In jth loop');
             // Change color for the current comparision (in consideration for min_index)
-            ele[j].style.background = 'red';
+            ele[j].style.background = '#dc3545';
 
             await waitforme(delay);
             if(parseInt(ele[j].style.height) < parseInt(ele[min_index].style.height)){
                 console.log('In if condition height comparision');
                 if(min_index !== i){
                     // new min_index is found so change prev min_index color back to normal
-                    ele[min_index].style.background = 'cyan';
+                    ele[min_index].style.background = '#0d6efd';
                 }
                 min_index = j;
             } 
             else{
                 // if the currnent comparision is more than min_index change is back to normal
-                ele[j].style.background = 'cyan';
+                ele[j].style.background = '#0d6efd';
             }   
         }
         await waitforme(delay);
         swap(ele[min_index], ele[i]);
         // change the min element index back to normal as it is swapped 
-        ele[min_index].style.background = 'cyan';
+        ele[min_index].style.background = '#0d6efd';
         // change the sorted elements color to green
-        ele[i].style.background = 'green';
+        ele[i].style.background = '#198754';
     }
 }
 
@@ -38,9 +38,9 @@ const selectionSortbtn = document.querySelector(".selectionSort");
 selectionSortbtn.addEventListener('click', async function(){
     disableSortingBtn();
     disableSizeSlider();
-    disableNewArrayBtn();
+    //disableNewArrayBtn();
     await selection();
     enableSortingBtn();
     enableSizeSlider();
-    enableNewArrayBtn();
+    //enableNewArrayBtn();
 });
