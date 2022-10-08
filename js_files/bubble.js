6b91c000ab26fcae1cf62ce70da73c2f9243abde
bansaldhruv0809 <84713243+bansaldhruv0809@users.noreply.github.com>
2022-10-08 18:57:44 +0530

    Update bubble.js

--- js_files/bubble.js
+++ js_files/bubble.js
@@ -5,28 +5,28 @@ async function bubble() {
         console.log('In ith loop');
         for(let j = 0; j < ele.length-i-1; j++){
             console.log('In jth loop');
-            ele[j].style.background = 'blue';
-            ele[j+1].style.background = 'blue';
+            ele[j].style.background = '#0d6efd';
+            ele[j+1].style.background = '#0d6efd';
             if(parseInt(ele[j].style.height) > parseInt(ele[j+1].style.height)){
                 console.log('In if condition');
                 await waitforme(delay);
                 swap(ele[j], ele[j+1]);
             }
-            ele[j].style.background = 'cyan';
-            ele[j+1].style.background = 'cyan';
+            ele[j].style.background = '#dc3545';
+            ele[j+1].style.background = '#dc3545';
         }
-        ele[ele.length-1-i].style.background = 'green';
+        ele[ele.length-1-i].style.background = '#198754';
     }
-    ele[0].style.background = 'green';
+    ele[0].style.background = '#198754';
 }
 
 const bubSortbtn = document.querySelector(".bubbleSort");
 bubSortbtn.addEventListener('click', async function(){
     disableSortingBtn();
     disableSizeSlider();
-    disableNewArrayBtn();
+    //disableNewArrayBtn();
     await bubble();
     enableSortingBtn();
     enableSizeSlider();
-    enableNewArrayBtn();
+    //enableNewArrayBtn();
 });
