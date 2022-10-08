d65e9a4466c49c22942ab8ea0dc55e61b5d450c3
bansaldhruv0809 <84713243+bansaldhruv0809@users.noreply.github.com>
2022-10-08 18:58:24 +0530

    Update insertion.js

--- js_files/insertion.js
+++ js_files/insertion.js
@@ -2,20 +2,20 @@ async function insertion(){
     console.log('In insertion()');
     const ele = document.querySelectorAll(".bar");
     // color
-    ele[0].style.background = 'green';
+    ele[0].style.background = '#198754';
     for(let i = 1; i < ele.length; i++){
         console.log('In ith loop');
         let j = i - 1;
         let key = ele[i].style.height;
         // color
-        ele[i].style.background = 'blue';
+        ele[i].style.background = '#0d6efd';
 
         await waitforme(delay);
 
         while(j >= 0 && (parseInt(ele[j].style.height) > parseInt(key))){
             console.log('In while loop');
             // color
-            ele[j].style.background = 'blue';
+            ele[j].style.background = '#0d6efd';
             ele[j + 1].style.height = ele[j].style.height;
             j--;
 
@@ -23,12 +23,12 @@ async function insertion(){
 
             // color
             for(let k = i; k >= 0; k--){
-                ele[k].style.background = 'green';
+                ele[k].style.background = '#198754';
             }
         }
         ele[j + 1].style.height = key;
         // color
-        ele[i].style.background = 'green';
+        ele[i].style.background = '#198754';
     }
 }
 
@@ -36,11 +36,11 @@ const inSortbtn = document.querySelector(".insertionSort");
 inSortbtn.addEventListener('click', async function(){
     disableSortingBtn();
     disableSizeSlider();
-    disableNewArrayBtn();
+    //disableNewArrayBtn();
     await insertion();
     enableSortingBtn();
     enableSizeSlider();
-    enableNewArrayBtn();
+    //enableNewArrayBtn();
 });
 
 
