392beaca59c0fd4d6f102d5f99805de4bd926c85
bansaldhruv0809 <84713243+bansaldhruv0809@users.noreply.github.com>
2022-10-08 19:38:38 +0530

    Update sorting.js

--- js_files/sorting.js
+++ js_files/sorting.js
@@ -88,7 +88,7 @@ function createNewArray(noOfBars = 60) {
     // creating an array of random numbers 
     array = [];
     for (let i = 0; i < noOfBars; i++) {
-        array.push(Math.floor(Math.random() * 190) + 1);
+        array.push(Math.floor(Math.random() * 200) + 1);
     }
     console.log(array);
 
